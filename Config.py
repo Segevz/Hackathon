2f5f350669ab1c23f107226a55da4a41c84ad76a
Nadav Miran <nadav@myfanzone.com>
2018-04-26 18:21:25 +0300

    small changes

--- Config.py
+++ Config.py
@@ -1,6 +1,7 @@
 
 
 schools = {1:"IDC", 2:"TAU", 3:"HUJI", 4:"BGU"}
-USERS_LOC = ""
+USERS_LOC = "C:\\Users\\Fanzone\\Desktop\\users.json"
+SCHOOLS_LOC = "C:\\Users\\Fanzone\\Desktop\\schools.json"
 COURSES_LOC = ""
 id = 0
