9d8a7cbfb0db7004964f4d90cab16e094fc5cd07
Nadav Miran <nadav@myfanzone.com>
2018-04-26 16:22:00 +0300

    added lists file and updated JsonHandler

--- JsonHandler.py
+++ JsonHandler.py
@@ -1,5 +1,8 @@
 import json
+import ast
+import Lists
 
+# USER
 def add_user():
     return false
 
@@ -8,17 +11,29 @@ def get_users_query(query):
     return false
 
 
-def get_user (id):
+def get_user(id):
+    all_users = ""
     return user
 
 
-def update_user (query):
+def update_user(query):
     return
 
-
-def get_course_query (school):
+# COURSE
+def get_course_query(school):
     return queryset
 
+# SCHOOL
+def get_all_schools():
+    return Lists.schools
 
 
-
+# JSON
+def read_json_file(str):
+    # data = open("C:\\Users\\Fanzone\\Desktop\\test.json")
+    data = open(str)
+    data = eval(data.read())
+    data = json.dumps(data)
+    data = json.loads(data)
+    data = ast.literal_eval(json.dumps(data))
+    return data
