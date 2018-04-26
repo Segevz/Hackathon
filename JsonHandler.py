48ca060c830daae62065bef3479d3fc2424915ba
Nadav Miran <nadav@myfanzone.com>
2018-04-26 15:38:31 +0300

    added skeleton for server

---
+++ JsonHandler.py
@@ -0,0 +1,24 @@
+import json
+
+def add_user():
+    return false
+
+
+def get_users_query(query):
+    return false
+
+
+def get_user (id):
+    return user
+
+
+def update_user (query):
+    return
+
+
+def get_course_query (school):
+    return queryset
+
+
+
+
